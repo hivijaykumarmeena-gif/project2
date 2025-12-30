7981cacb2eebe67280e90b8aedc4dbd69ed2f010
Vijay Kumar Meena <hivijaykumarmeena@gmail.com>
2025-12-30 12:56:43 +0530

    Update menu.js

--- script/menu.js
+++ script/menu.js
@@ -38,7 +38,34 @@ fetch("header.html")
 					menu.classList.toggle("mobile-open");
 				});
 			}
-			h
+
+			// sabse pehle --> menu items nikalne padenge
+			const menuList = document.querySelectorAll(".menu .menubar ul li");
+			
+			menuList.forEach(li => {
+				// second step --> voh menu items filter karna jisme submenu ho
+				const submenuExists = li.querySelectorAll(".submenu");
+				if (submenuExists) {
+					// third step --> identify karna jis menu item pr click hua ho
+					const toggle = document.createElement("button");
+					toggle.className = "submenu-toggle";
+					toggle.setAttribute("aria-expanded", "false");
+					toggle.innerHTML = '<span style="display:inline-block; transform:rotate(90deeg);">&#9656;</span>';
+
+					// dummy li add in the end of the list
+					const firstLink = li.querySelector("a");
+
+					// fifth step --> submenu display karna jo menu item click hua ho
+					submenuExists.addEventListener("click", function (ev) {
+						ev.stopPropagation();
+						const isOpen = li.classList.toggle("open-submenu");
+						toggle.setAttribute("aria-expanded", isOpen? "true" : "false");
+					});
+					// fouth step --> close already open / close all submenu
+					
+				}
+			});
+			
 		}
 	});
 
